df7dbb8ccb92e73a9dc70e1a01297421cff07073
Taynara <taynaraax0@hotmail.com>
2022-06-08 22:16:09 -0300

    app completo
    app completo

---
+++ app-notas/www/js/js.js
@@ -0,0 +1,10 @@
+function salvarAnotocao(){
+    novaAnotacao = document.getElementById("anotacao").value;
+    document.getElementById("anotacoes-salvas").innerHTML += "<p>" + novaAnotacao + "</p>";
+    document.getElementById("anotacao").value = "";
+    alert("Anotação salva com sucesso");
+}
+
+document.getElementById("salvar").addEventListener("click", function(){
+    salvarAnotocao();
+});
